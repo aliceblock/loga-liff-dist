71b6c83b299840169aea98105962b7aefe411651
Saran Jenjirawat <registbacktown@gmail.com>
2020-05-05 01:32:19 +0700

    deploy

---
+++ precache-manifest.ed941351735c8d8588bfff9b66bba04a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f365813757b6d388d8c4",
+    "url": "/css/app.601d2ada.css"
+  },
+  {
+    "revision": "93fe34ac94297c6ec9a60c9147366a8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3de3a542d5eaafc94a07",
+    "url": "/js/about.503d6d25.js"
+  },
+  {
+    "revision": "f365813757b6d388d8c4",
+    "url": "/js/app.7e40017f.js"
+  },
+  {
+    "revision": "725e71275a0fa276d7db",
+    "url": "/js/chunk-vendors.98006312.js"
+  },
+  {
+    "revision": "40949b0152a82246589e2f978fe981a5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
